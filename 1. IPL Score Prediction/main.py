3268203509b541240486a209343c04a9811ef415
Krishna-1996 <1904krishna@gmail.com>
2024-07-16 07:17:54 +0530

    added

--- 1. IPL Score Prediction/main.py
+++ 1. IPL Score Prediction/main.py
@@ -7,6 +7,6 @@ from sklearn import preprocessing
 import tensorflow as tf
 import keras
 
-
+# 2. Import Dataset
 df = pd.read_csv('E:/Machine Learning/Machine Learning/Machine_Learning_Projects/Machine_Learning_Projects/1. IPL Score Prediction/ipl_data.csv')
 print(df.head())
