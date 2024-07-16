9c5c1a7b2996b54ffd25b72b59d205cb45dc25b1
Krishna-1996 <1904krishna@gmail.com>
2024-07-16 07:28:08 +0530

    added

--- 1. IPL Score Prediction/main.py
+++ 1. IPL Score Prediction/main.py
@@ -8,7 +8,9 @@ import tensorflow as tf
 import keras
 
 # 2. Import Dataset
-df = pd.read_csv('E:/Machine Learning/Machine Learning/Machine_Learning_Projects/Machine_Learning_Projects/1. IPL Score Prediction/ipl_data.csv')
-print(df.head())
+ipl = pd.read_csv('E:/Machine Learning/Machine Learning/Machine_Learning_Projects/Machine_Learning_Projects/1. IPL Score Prediction/ipl_data.csv')
+print(ipl.head())
 
 # 3. Data Pre-Processing
+# 3.1 Drop Unnecessary Columns
+df = ipl.drop(['date', 'runs', 'wickets', 'overs', 'runs_last_5', 'wickets_last_5','mid', 'striker', 'non-striker'], axis =1)
