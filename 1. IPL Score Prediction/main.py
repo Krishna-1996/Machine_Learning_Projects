82c9df06f4371774b5b1db9d2149cca9b71a78a2
Krishna-1996 <1904krishna@gmail.com>
2024-07-16 07:17:03 +0530

    added

--- 1. IPL Score Prediction/main.py
+++ 1. IPL Score Prediction/main.py
@@ -4,6 +4,7 @@ from matplotlib.pyplot import plt
 import seaborn as sns
 from sklearn import preprocessing
 import tensorflow as tf
+import keras
 
 
 df = pd.read_csv('E:/Machine Learning/Machine Learning/Machine_Learning_Projects/Machine_Learning_Projects/1. IPL Score Prediction/ipl_data.csv')
