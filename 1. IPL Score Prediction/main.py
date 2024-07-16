4fee9f8f232b4c550c7147baf98171a9ed4a66a7
Krishna-1996 <1904krishna@gmail.com>
2024-07-16 07:44:45 +0530

    added

--- 1. IPL Score Prediction/main.py
+++ 1. IPL Score Prediction/main.py
@@ -1,15 +1,15 @@
 # 1. Import necessary Libraries:
 import pandas as pd
 import numpy as np
-from matplotlib.pyplot import plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import preprocessing
-import tensorflow as tf
-import keras
+import keras 
+import tensorflow as tf 
 
 # 2. Import Dataset
 ipl = pd.read_csv('E:/Machine Learning/Machine Learning/Machine_Learning_Projects/Machine_Learning_Projects/1. IPL Score Prediction/ipl_data.csv')
-print(ipl.head())
+'''print(ipl.head())'''
 
 # 3. Data Pre-Processing
 # 3.1 Drop Unnecessary Columns
