8d5eadd725666f501d8c3579e98204d12dd4c4ca
Krishna-1996 <1904krishna@gmail.com>
2024-07-16 07:15:21 +0530

    added

--- 1. IPL Score Prediction/main.py
+++ 1. IPL Score Prediction/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from matplotlib.pyplot import plot
 
 df = pd.read_csv('E:/Machine Learning/Machine Learning/Machine_Learning_Projects/Machine_Learning_Projects/1. IPL Score Prediction/ipl_data.csv')
-print(df.head()
+print(df.head())
