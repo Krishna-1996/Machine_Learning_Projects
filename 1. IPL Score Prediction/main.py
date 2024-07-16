425eeb8b003f2cfaf53a6fd6f6fc260c4b654f81
Krishna-1996 <1904krishna@gmail.com>
2024-07-16 07:21:57 +0530

    added

--- 1. IPL Score Prediction/main.py
+++ 1. IPL Score Prediction/main.py
@@ -10,3 +10,5 @@ import keras
 # 2. Import Dataset
 df = pd.read_csv('E:/Machine Learning/Machine Learning/Machine_Learning_Projects/Machine_Learning_Projects/1. IPL Score Prediction/ipl_data.csv')
 print(df.head())
+
+# 3. Data Pre-Processing
