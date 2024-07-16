144ee837ef29146ffff63911ccc258c622987233
Krishna-1996 <1904krishna@gmail.com>
2024-07-16 07:16:14 +0530

    added

--- 1. IPL Score Prediction/main.py
+++ 1. IPL Score Prediction/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-from matplotlib.pyplot import plot
+from matplotlib.pyplot import plt
+import seaborn as sns
 
 
 df = pd.read_csv('E:/Machine Learning/Machine Learning/Machine_Learning_Projects/Machine_Learning_Projects/1. IPL Score Prediction/ipl_data.csv')
