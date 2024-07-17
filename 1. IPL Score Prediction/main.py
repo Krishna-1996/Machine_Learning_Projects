037dc1936cb9b7d84b951e33ab084fa39669a429
Krishna-1996 <1904krishna@gmail.com>
2024-07-17 06:56:06 +0530

    added

--- 1. IPL Score Prediction/main.py
+++ 1. IPL Score Prediction/main.py
@@ -54,7 +54,10 @@ model = keras.Sequential([
     keras.layers.Input(shape=(x_train_scalar.shape[1],)),
     # layer 1 relu activation wih 512 neurons
     keras.layer.Dense(512,activation='relu'),
+    # layer 2 relu activation wih 216 neurons
     keras.layer.Dense(216,activation='relu'),
+    # layer 3 linear activation wih 1 neurons
+    # the output will be real number 
     keras.layer.Dense(1, activation='linear')
 
 
