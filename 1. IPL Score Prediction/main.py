8ab40279242da9730c2169cf9304e5c115dcb048
Krishna-1996 <1904krishna@gmail.com>
2024-07-19 08:08:02 +0530

    added

--- 1. IPL Score Prediction/main.py
+++ 1. IPL Score Prediction/main.py
@@ -51,7 +51,7 @@ model = keras.Sequential([
     keras.layers.Dense(1, activation='linear')
 ])
 
-# Compile the model
+# Compile the mode
 optimizer = keras.optimizers.Adam(learning_rate=0.001)
 model.compile(optimizer=optimizer, loss='huber', metrics=['mae'])
 
