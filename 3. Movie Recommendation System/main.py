b5f5fbab2858ebcfeb06250b4c5377e21b6e0baf
Krishna-1996 <1904krishna@gmail.com>
2024-09-28 16:50:38 +0530

    added

--- 3. Movie Recommendation System/main.py
+++ 3. Movie Recommendation System/main.py
@@ -1,4 +1,6 @@
+# Download the Dataset:
 import pandas as pd 
-data = pd.read_csv("E://Machine Learning/Machine Learning/Machine_Learning_Projects/Machine_Learning_Projects/3. Movie Recommendation System/tmdb_5000_credits.csv")
 
+# Explore the Data:
+data = pd.read_csv("E://Machine Learning/Machine Learning/Machine_Learning_Projects/Machine_Learning_Projects/3. Movie Recommendation System/tmdb_5000_credits.csv")
 print(data.head())
