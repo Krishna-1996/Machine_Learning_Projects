28c28d390ce338160ad497a234e49ef9421afa29
Krishna-1996 <1904krishna@gmail.com>
2024-09-28 16:38:52 +0530

    added

---
+++ 3. Movie Recommendation System/main.py
@@ -0,0 +1,4 @@
+import pandas as pd 
+data = pd.read_csv("E://Machine Learning/Machine Learning/Machine_Learning_Projects/Machine_Learning_Projects/3. Movie Recommendation System/tmdb_5000_credits.csv")
+
+print(data.head())
