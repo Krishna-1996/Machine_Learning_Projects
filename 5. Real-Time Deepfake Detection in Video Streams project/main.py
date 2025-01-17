a78ec2dcb716de520836d7c02a9d50ccae5cb33a
Krishna-1996 <1904krishna@gmail.com>
2025-01-17 11:07:40 +0000

    new project

--- 5. Real-Time Deepfake Detection in Video Streams project/main.py
+++ 5. Real-Time Deepfake Detection in Video Streams project/main.py
@@ -1,13 +1,17 @@
 # Database: D:/MSc. Project DeepFake Detection Datasets/Celeb-DF-v1
 
 # %%
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 from tensorflow import keras
-import tensorflow as tf
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 import imageio
+import random
 import cv2
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
