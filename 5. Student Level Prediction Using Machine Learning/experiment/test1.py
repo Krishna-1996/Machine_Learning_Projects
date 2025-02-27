fc299973ddd0c78adb842853009b3a833b56d2cd
Krishna-1996 <1904krishna@gmail.com>
2025-02-27 17:24:34 +0000

    editing

--- 5. Student Level Prediction Using Machine Learning/experiment/test1.py
+++ 5. Student Level Prediction Using Machine Learning/experiment/test1.py
@@ -4,8 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_tensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
