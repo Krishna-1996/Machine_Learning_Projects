31f06010bf74a92e3e8e2985a9da752ce47a9783
Krishna-1996 <1904krishna@gmail.com>
2025-05-18 07:50:07 +0100

    editing

---
+++ 6. Prompt Engineering/Testing_Medical Data/mytest1.py
@@ -0,0 +1,39 @@
+# %%
+# Step 1: Import necessary libraries
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import nltk
+import spacy
+from spacy.tokens import DocBin
+from tqdm import tqdm
+
+# %%
+# Step 2:
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+
+# %%
+# Step 3:
+
+
+
+# %%
+# Step 4:
+
+
+
+# %%
+# Step 5:
+
+
+
+# %%
+# Step 6:
+
