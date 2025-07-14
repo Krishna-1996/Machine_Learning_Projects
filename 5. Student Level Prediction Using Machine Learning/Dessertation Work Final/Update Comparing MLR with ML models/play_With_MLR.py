047add42fdd9d6802cf3ba46994e31aba4024934
Krishna-1996 <1904krishna@gmail.com>
2025-07-14 18:00:06 +0100

    Editing

--- 5. Student Level Prediction Using Machine Learning/Dessertation Work Final/Update Comparing MLR with ML models/play_With_MLR.py
+++ 5. Student Level Prediction Using Machine Learning/Dessertation Work Final/Update Comparing MLR with ML models/play_With_MLR.py
@@ -13,8 +13,6 @@ os.makedirs("results", exist_ok=True)
 excel_writer = pd.ExcelWriter("results/results_summary.xlsx", engine="openpyxl")
 
 
-
-
 # %%
 
 # Step 1: Load and Clean Data
@@ -25,34 +23,13 @@ from sklearn.preprocessing import LabelEncoder
 file_path = 'The_Student_Dataset.csv'
 df = pd.read_csv(file_path)
 df.drop(columns=['Age as of Academic Year 17/18','Current Year (17/18)',
-                 'Proposed Year/Grade (18/19)','Current School ','Current Curriculum ','Previous year/Grade '], inplace=True)
+                 'Proposed Year/Grade (18/19)','Current School ','Current Curriculum ','Previous year/Grade ',
+                 'Gender','Previous Curriculum (17/18)2'], inplace=True)
 
 # 1.2 Clean column names: remove leading/trailing spaces, replace spaces with underscores, remove non-alphanumeric characters
 df.columns = df.columns.str.strip()  # Remove leading/trailing spaces from column names
 df.columns = df.columns.str.replace(r'\s+', '_', regex=True)  # Replace spaces with underscores
 df.columns = df.columns.str.replace(r'[^a-zA-Z0-9_]', '', regex=True)  # Remove non-alphanumeric characters
-
-# 1.3 Clean unique data: Standardize categories and remove extra spaces
-def clean_column_data(column):
-    column = column.str.strip()  # Remove leading/trailing spaces
-    column = column.replace({
-        'Year System' : 'Year System', 'Year System ' : 'Year System',
-        'Grade System' : 'Grade System', 'Grade system' : 'Grade System',
-    }, regex=True)
-    return column
-
-# 1.4 Apply the cleaning function to all categorical columns
-for col in df.columns:
-    if df[col].dtype == 'object':  # Only clean non-numeric columns
-        df[col] = clean_column_data(df[col])
-
-# 1.5 Filter rows based on curriculum (only American and British)
-valid_curricula = ['American', 'British']
-df = df[df['Previous_Curriculum_17182'].isin(valid_curricula)]
-
-# 1.6 Modify 'Year_of_Admission' based on 'Current_School' column
-df['Year_of_Admission'] = df['Year_of_Admission'].replace({'School 1 Current Student':'Current Student'})
-df['Year_of_Admission'] = df['Year_of_Admission'].replace({'School 2 Current Student':'Current Student'})
 
 
 # %%
@@ -73,17 +50,12 @@ for col in df.columns:
 categorical_columns = df.select_dtypes(include=['object']).columns
 label_encoders = {}
 
-# 3.1 Remove rows where 'Year_of_Admission' contains "New Admission 18/19"
-df = df[df['Year_of_Admission'] != 'New Admission 18/19']
-
 for col in categorical_columns:
     le = LabelEncoder()
     df[col] = le.fit_transform(df[col])  # Convert categorical to numerical
     label_encoders[col] = le  # Save the encoder for future reference
 
-# Now just drop the column Year_of_Admission.
-df.drop(columns=['Year_of_Admission'], inplace=True)
-# 3.2 Save the preprocessed data and mappings
+# 3.1 Save the preprocessed data and mappings
 output_file_path = 'The_Student_Dataset_Preprocessed.xlsx'
 mapping_data = []
 
@@ -93,7 +65,7 @@ for col, le in label_encoders.items():
 
 mapping_df = pd.DataFrame(mapping_data)
 
-# 3.3 Write the cleaned data and mapping data to an Excel file
+# 3.2 Write the cleaned data and mapping data to an Excel file
 with pd.ExcelWriter(output_file_path) as writer:
     df.to_excel(writer, sheet_name='Data', index=False)
     mapping_df.to_excel(writer, sheet_name='Mappings')
